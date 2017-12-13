4c9f8e3b2ceadacb6d598136289ef87bd453d86e
npearl95 <npearl1995@gmail.com>
2017-12-13 13:37:02 -0500

    add password validation

--- javascript/myJavaScript.js
+++ javascript/myJavaScript.js
@@ -22,17 +22,64 @@
     }
 
 }*/
-function validateLogInForm() {
-    //check for empty "text" input field
-    var inp = document.getElementsByTagName('input');
-    for(var i in inp){
-        if(inp[i].type == "email"||inp[i].type == "password"){
-            if(inp[i].value==""){
-                alert(inp[i].placeholder +" is missing.");
-                //inp[i].focus(); //this will erase the field
-                return false;
-                break;
-            }
-        }
+function validatePassword() {
+    var input=document.getElementById("password");
+    var lowerCase=document.getElementById("lowerCase");
+    var upperCase=document.getElementById("upperCase");
+    var number=document.getElementById("number");
+    var length=document.getElementById("length");
+    //Onfocus: Displace message box when user click on passworkd id
+    document.getElementById("message").style.display = "block";
+    //Onblur: Hide message box
+    input.onblur=function(){
+        document.getElementById("message").style.display="none";
+    }
+
+    // When the user starts to type something inside the password field
+    input.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(input.value.match(lowerCaseLetters)) {  
+        lowerCase.classList.remove("invalid");
+        lowerCase.classList.add("valid");
+    } else {
+        lowerCase.classList.remove("valid");
+        lowerCase.classList.add("invalid");
+    }
+    // Validate upperCase letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(input.value.match(upperCaseLetters)) {  
+        upperCase.classList.remove("invalid");
+        upperCase.classList.add("valid");
+    } else {
+        upperCase.classList.remove("valid");
+        upperCase.classList.add("invalid");
+    }
+    // validation for number
+    var numberFormat=/[0-9]/g;
+    if(input.value.match(numberFormat)){
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    }else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+      // Validate length
+  if(input.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+}
+
+function showPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
     }
 }
